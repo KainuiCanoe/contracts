a625001a8fb5a34f6f5ae1b3092d8db8c3978a99
Brian Taylor <brian@resolvingarchitecture.com>
2017-06-30 17:41:01 -0400

    Initial

---
+++ token/CrowdSaleToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.0;
+
+contract CrowdSaleToken is ERC20BasicToken {
+
+    string public name = "CrowdSaleToken";
+
+    function CrowdSaleToken(){
+
+    }
+
+    function createTokens(address recipient) payable {
+
+    }
+
+}
---
+++ token/ERC20Token.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.0;
+
+contract ERC20Token {
+    function totalSupply() constant returns (uint totalSupply);
+    function balanceOf(address owner) constant returns (uint balance);
+    function transfer(address to, uint value) returns (bool success);
+    function transferFrom(address from, address to, uint value) returns (bool success);
+    function approve(address spender, uint value) returns (bool success);
+    function allowance(address owner, address spender) constant returns (uint remaining);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
